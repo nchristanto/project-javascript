556299ee88ccbdcb7d035051ef6bffcb20deed5e
Nanda D Christanto <christanto.nanda@gmail.com>
2019-04-04 16:50:13 +0700

    link index.html file to index.css and index.js clear

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Javascript
+
+### Project to experiment Javascript
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+console.log("Hello Warning");
+console.error("Hello Error");
+console.info("Hello Info");
+
+alert("Hello Alert");
